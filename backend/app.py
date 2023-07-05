27a60d12a848fc929a1e7f148ffc176f030bdcdb
EfstathiosZaragkas <95194233+EfstathiosZaragkas@users.noreply.github.com>
2023-07-05 22:45:40 +0300

    Update backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, send_file
 from flask_cors import CORS
 from PIL import Image
 import numpy as np
@@ -7,12 +7,14 @@ from mtcnn import MTCNN
 from numpy import asarray
 import tensorflow as tf
 import h5py
-import json
-from flask import jsonify
-
+import os
+import random
 
 app = Flask(__name__)
 CORS(app, origins='http://localhost:3000')
+
+MUSIC_FOLDER_CLASSICAL = 'Classical Music/'
+MUSIC_FOLDER_MODERN = 'Modern Music/'
 
 @app.route('/')
 def home():
@@ -20,11 +22,9 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Check if a file was submitted in the request
     if 'image' not in request.files:
         return jsonify({'error': 'No image file uploaded'})
 
-    # Retrieve the uploaded file
     image_file = request.files['image']
     save_path = 'uploads/' + image_file.filename
     image_file.save(save_path)
@@ -32,21 +32,19 @@ def upload():
     face_image = Image.fromarray(face_pixels, 'RGB')
     face_image.save("cropt_photos/face.jpg")
 
-    # Load the model from an HDF5 file
     model_path = 'models_mobilenet/mobilenet_model_4_bs64_quad.hdf5'
     with h5py.File(model_path, 'r') as file:
         loaded_model = tf.keras.models.load_model(file, compile=False)
 
-    # Load and preprocess the image
     image_path = 'cropt_photos/face.jpg'
     image = Image.open(image_path)
     image = image.resize((48, 48))
-    image = np.array(image) / 255.0  # Normalize the image
+    image = np.array(image) / 255.0
     predictions = loaded_model.predict(np.expand_dims(image, axis=0))
     predicted_class_index = np.argmax(predictions, axis=1)
     labels = {0: 'Q1', 1: 'N', 2: 'Q3', 3: 'Q2'}
     predicted_labels = [labels[k] for k in predicted_class_index]
-    print(predicted_labels)  # Print the predicted labels
+    print(predicted_labels)
 
     return jsonify({'labels': predicted_labels})
 
@@ -61,6 +59,23 @@ def extract_face(filename, required_size=(48, 48)):
     image = image.resize(required_size)
     face_array = asarray(image)
     return face_array
+
+@app.route('/song', methods=['POST'])
+def song():
+    selected_option = request.form.get('option')
+    label = request.form.get('label')
+    print("label: " + label)
+
+    if selected_option == 'classical':
+        music_folder = os.path.join(MUSIC_FOLDER_CLASSICAL, label)
+    else:
+        music_folder = os.path.join(MUSIC_FOLDER_MODERN, label)
+
+    music_files = os.listdir(music_folder)
+    selected_song = random.choice(music_files)
+    print("selected song: " + selected_song)
+
+    return send_file(os.path.join(music_folder, selected_song), as_attachment=True)
 
 if __name__ == '__main__':
     app.run()
